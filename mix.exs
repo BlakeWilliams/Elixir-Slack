a677000705d320d403ec0690ca82ef23958eb0c2
Blake Williams <blake@blakewilliams.me>
2016-01-03 18:37:44 -0500

    Update earmark and ex_Doc
    Documentation would not compile with an older version of `ex_doc` any
    longer. This updates `earmark` and `ex_doc` to the latest versions.

--- mix.exs
+++ mix.exs
@@ -21,8 +21,8 @@ defmodule Slack.Mixfile do
     [{:httpoison, "~> 0.8.0"},
      {:exjsx, "~> 3.1.0"},
      {:websocket_client, github: "jeremyong/websocket_client"},
-     {:earmark, "~> 0.1", only: :dev},
-     {:ex_doc, "~> 0.6", only: :dev}]
+     {:earmark, "~> 0.2.0", only: :dev},
+     {:ex_doc, "~> 0.11", only: :dev}]
   end
 
   def docs do

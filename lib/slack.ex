0b2fd0e3b4c19e5fc752a1530e6ab759793f86c7
James Edward Gray II <james@graysoftinc.com>
2016-04-22 14:19:45 -0500

    Support sending to an already open direct message channel and add lookup helpers for channels and users.

--- lib/slack.ex
+++ lib/slack.ex
@@ -102,6 +102,7 @@ defmodule Slack do
           channels: rtm_list_to_map(rtm.channels),
           groups: rtm_list_to_map(rtm.groups),
           users: rtm_list_to_map(rtm.users),
+          ims: rtm_list_to_map(rtm.ims)
         }
 
         {:ok, state} = handle_connect(slack, state)
@@ -157,19 +158,81 @@ defmodule Slack do
     end
   end
 
-  @doc """
-  Sends `text` to `channel` for the given `slack` connection.
+  @doc ~S"""
+  Turns a string like `"@USER_NAME"` into the ID that Slack understands (`"U…"`).
   """
-  def send_message(text, "#" <> channel_name, slack) do
-    channel_id =
-      slack.channels
-      |> Map.values
-      |> Enum.find(fn channel -> channel.name == channel_name end)
-      |> Map.get(:id)
+  def lookup_user_id("@" <> user_name, slack) do
+    slack.users
+    |> Map.values
+    |> Enum.find(fn user -> user.name == user_name end)
+    |> Map.get(:id)
+  end
+
+  @doc ~S"""
+  Turns a string like `"@USER_NAME"` or a user ID (`"U…"`) into the ID for the
+  direct message channel of that user (`"D…"`).  `nil` is returned if a direct
+  message channel has not yet been opened.
+  """
+  def lookup_direct_message_id(user = "@" <> _user_name, slack) do
+    user
+    |> lookup_user_id(slack)
+    |> lookup_direct_message_id(slack)
+  end
+  def lookup_direct_message_id(user_id, slack) do
+    slack.ims
+    |> Map.values
+    |> Enum.find(fn direct_message -> direct_message.user == user_id end)
+    |> Map.get(:id)
+  end
+
+  @doc ~S"""
+  Turns a string like `"@CHANNEL_NAME"` into the ID that Slack understands
+  (`"C…"`).
+  """
+  def lookup_channel_id("#" <> channel_name, slack) do
+    slack.channels
+    |> Map.values
+    |> Enum.find(fn channel -> channel.name == channel_name end)
+    |> Map.get(:id)
+  end
+
+  @doc ~S"""
+  Turns a Slack user ID (`"U…"`) or direct message ID (`"D…"`) into a string in
+  the format "@USER_NAME".
+  """
+  def lookup_user_name(direct_message_id = "D" <> _id, slack) do
+    lookup_user_name(slack.ims[direct_message_id].user, slack)
+  end
+  def lookup_user_name(user_id = "U" <> _id, slack) do
+    "@" <> slack.users[user_id].name
+  end
+
+  @doc ~S"""
+  Turns a Slack channel ID (`"C…"`) into a string in the format "#CHANNEL_NAME".
+  """
+  def lookup_channel_name(channel_id = "C" <> _id, slack) do
+    "#" <> slack.channels[channel_id].name
+  end
+
+  @doc """
+  Sends `text` to `channel` for the given `slack` connection.  `channel` can be
+  a string in the format of `"#CHANNEL_NAME"`, `"@USER_NAME"`, or any ID that
+  Slack understands.
+  """
+  def send_message(text, channel = "#" <> channel_name, slack) do
+    channel_id = lookup_channel_id(channel, slack)
     if channel_id do
       send_message(text, channel_id, slack)
     else
       raise ArgumentError, "channel ##{channel_name} not found"
+    end
+  end
+  def send_message(text, user = "@" <> user_name, slack) do
+    direct_message_id = lookup_direct_message_id(user, slack)
+    if direct_message_id do
+      send_message(text, direct_message_id, slack)
+    else
+      raise ArgumentError, "direct message channel for ##{user_name} not found"
     end
   end
   def send_message(text, channel, slack) do

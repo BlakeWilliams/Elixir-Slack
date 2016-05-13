3fe858a31843460d4f645f854df4da241aaf6a3f
Chris Keele <dev@chriskeele.com>
2016-05-13 10:15:21 -0500

    Handles general json parse errors and special throttled case.

--- lib/slack/rtm.ex
+++ lib/slack/rtm.ex
@@ -1,12 +1,22 @@
+defmodule JSX.DecodeError do
+  defexception [:reason, :string]
+  
+  def message(%JSX.DecodeError{reason: reason, string: string}) do
+    "JSX could not decode string for reason: `:#{reason}`, string given:\n#{string}"
+  end
+end
+
 defmodule Slack.Rtm do
   @moduledoc false
   @url "https://slack.com/api/rtm.start?token="
 
   def start(token) do
     case HTTPoison.get(@url <> token) do
-      {:ok, response} ->
-        json = JSX.decode!(response.body, [{:labels, :atom}])
-        {:ok, json}
+      {:ok, %HTTPoison.Response{body: body} } ->
+        case JSX.decode(body, [{:labels, :atom}]) do
+          {:ok, json}       -> {:ok, json}
+          {:error, reason}  -> {:error, %JSX.DecodeError{reason: reason, string: body} }
+        end
       {:error, reason} -> {:error, reason}
     end
   end

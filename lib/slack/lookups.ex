702facf5b22c86bf3983988300d647acb489835a
Manuel Zubieta <mazubieta@gmail.com>
2017-03-09 08:26:40 -0600

    allow bot username lookup (#110)

--- lib/slack/lookups.ex
+++ lib/slack/lookups.ex
@@ -39,14 +39,17 @@ defmodule Slack.Lookups do
   end
 
   @doc ~S"""
-  Turns a Slack user ID (`"U…"`) or direct message ID (`"D…"`) into a string in
-  the format "@USER_NAME".
+  Turns a Slack user ID (`"U…"`), direct message ID (`"D…"`), or bot ID (`"B…"`)
+  into a string in the format "@USER_NAME".
   """
   def lookup_user_name(direct_message_id = "D" <> _id, slack) do
     lookup_user_name(slack.ims[direct_message_id].user, slack)
   end
   def lookup_user_name(user_id = "U" <> _id, slack) do
     "@" <> slack.users[user_id].name
+  end
+  def lookup_user_name(bot_id = "B" <> _id, slack) do
+    "@" <> slack.bots[bot_id].name
   end
 
   @doc ~S"""

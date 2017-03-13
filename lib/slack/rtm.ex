c764a1b61daf5bf98e9df1234c4480a5c7bf78f6
Dave Lucia <davelucianyc@gmail.com>
2017-03-13 19:44:15 -0400

    Handle RTM failure scenario (#113)
    * Handle RTM failure scenario
    * Drop elixir 1.2.0 tests
    * Handle error response from slack

--- lib/slack/rtm.ex
+++ lib/slack/rtm.ex
@@ -10,15 +10,22 @@ defmodule Slack.Rtm do
   @moduledoc false
 
   def start(token) do
-    url = Application.get_env(:slack, :url, "https://slack.com") <> "/api/rtm.start?token="
+    slack_url(token)
+    |> HTTPoison.get()
+    |> handle_response()
+  end
 
-    case HTTPoison.get(url <> token) do
-      {:ok, %HTTPoison.Response{body: body}} ->
-        case JSX.decode(body, [{:labels, :atom}]) do
-          {:ok, json}       -> {:ok, json}
-          {:error, reason}  -> {:error, %JSX.DecodeError{reason: reason, string: body}}
-        end
-      {:error, reason} -> {:error, reason}
+  defp handle_response({:ok, %HTTPoison.Response{body: body}}) do
+    case JSX.decode(body, [{:labels, :atom}]) do
+      {:ok, %{ok: true} = json} -> {:ok, json}
+      {:ok, %{error: reason}} -> {:error, "Slack API returned an error `#{reason}.\n Response: #{body}"}
+      {:error, reason} -> {:error, %JSX.DecodeError{reason: reason, string: body}}
+      _ -> {:error, "Invalid RTM response"}
     end
+  end
+  defp handle_response(error), do: error
+
+  defp slack_url(token) do
+    Application.get_env(:slack, :url, "https://slack.com") <> "/api/rtm.start?token=#{token}"
   end
 end

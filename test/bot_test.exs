818978d773bb21514932a355e5c1dd07c04e13bc
Matt Widmann <mgwidmann@gmail.com>
2017-06-18 10:39:23 -0400

    Allow RTM module to be configured so that tests may stub this out. (#122)
    * Allow RTM module to be configured so that tests may stub this out.
    
    * Remove unnecessary variable in tests.
    
    * Get rid of :"Elixir." namespace prefix from overridden name

--- test/bot_test.exs
+++ test/bot_test.exs
@@ -5,21 +5,22 @@ defmodule Slack.BotTest do
     use Slack
   end
 
-  test "init formats rtm results properly" do
-    rtm = %{
-      self: %{name: "fake"},
-      team: %{name: "Foo"},
-      bots: [%{id: "123"}],
-      channels: [%{id: "123"}],
-      groups: [%{id: "123"}],
-      users: [%{id: "123"}],
-      ims: [%{id: "123"}]
-    }
+  @rtm %{
+    url: "http://example.com",
+    self: %{name: "fake"},
+    team: %{name: "Foo"},
+    bots: [%{id: "123"}],
+    channels: [%{id: "123"}],
+    groups: [%{id: "123"}],
+    users: [%{id: "123"}],
+    ims: [%{id: "123"}]
+  }
 
+  test "init formats rtm results properly" do
     {:reconnect, %{slack: slack, bot_handler: bot_handler}}
       = Slack.Bot.init(%{
           bot_handler: Bot,
-          rtm: rtm,
+          rtm: @rtm,
           client: FakeWebsocketClient,
           token: "ABC",
           initial_state: nil,
@@ -33,5 +34,22 @@ defmodule Slack.BotTest do
     assert slack.groups   == %{"123" => %{id: "123"}}
     assert slack.users    == %{"123" => %{id: "123"}}
     assert slack.ims      == %{"123" => %{id: "123"}}
+  end
+
+  defmodule Stubs.Slack.Rtm do
+    def start(_token) do
+      {:ok, %{url: "http://www.example.com"}}
+    end
+  end
+
+  defmodule Stubs.Slack.WebsocketClient do
+    def start_link(_url, _module, _state, _options) do
+      {:ok, self()}
+    end
+  end
+
+  test "can configure the RTM module" do
+    Application.put_env(:slack, :rtm_module, Stubs.Slack.Rtm)
+    assert {:ok, _pid} = Slack.Bot.start_link(Bot, %{}, "token", %{client: Stubs.Slack.WebsocketClient})
   end
 end

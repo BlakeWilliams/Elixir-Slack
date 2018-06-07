bd6388702c133de6e565d28ac93f0ebeac357021
Mike Martinson <mike.j.martinson@gmail.com>
2018-06-07 15:58:41 -0700

    Add configurable web http client

---
+++ lib/slack/web/client.ex
@@ -0,0 +1,28 @@
+defmodule Slack.Web.Client do
+  @moduledoc """
+  Default http client used for all requests to web API.
+
+  All Slack RPC method calls are delivered via post and are dangerous by
+  default, raising on any HTTP response that doesn't contain a body field.
+
+  Parsed body data is returned unwrapped to the caller.
+
+  Additional error handling or response wrapping can be controlled as needed
+  by configuring a custom client module.
+
+  ```
+  config :slack, :web_http_client, YourApp.CustomClient
+  ```
+  """
+
+  def post!(url, body) do
+    url
+    |> HTTPoison.post!(body, [], opts())
+    |> Map.fetch!(:body)
+    |> Poison.Parser.parse!()
+  end
+
+  defp opts do
+    Application.get_env(:slack, :web_http_client_opts, [])
+  end
+end
--- lib/slack/web/web.ex
+++ lib/slack/web/web.ex
@@ -43,19 +43,21 @@ Enum.each(Slack.Web.get_documentation, fn({module_name, functions}) ->
         url = Application.get_env(:slack, :url, "https://slack.com")
 
         params = optional_params
-        |> Map.to_list
+        |> Map.to_list()
         |> Keyword.merge(required_params)
         |> Keyword.put_new(:token, get_token(optional_params))
         |> Enum.reject(fn {_, v} -> v == nil end)
 
-        %{body: body} = HTTPoison.post!(
+        perform!(
           "#{url}/api/#{unquote(doc.endpoint)}",
           params(unquote(function_name), params, unquote(arguments))
         )
-
-        Poison.Parser.parse!(body)
       end
     end)
+
+    defp perform!(url, body) do
+      Application.get_env(:slack, :web_http_client, Slack.Web.Client).post!(url, body)
+    end
 
     defp get_token(%{token: token}), do: token
     defp get_token(_), do: Application.get_env(:slack, :api_token)

3aa01d79be1622f76f2be18ab4adb17d6e25a09c
neiro <neiro.mail@gmail.com>
2018-08-03 22:08:37 +0300

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -22,13 +22,13 @@ defmodule Slack.Mixfile do
   end
 
   defp deps do
-    [{:httpoison, "~> 0.11"},
+    [{:httpoison, "~> 1.2"},
      {:websocket_client, "~> 1.2.4"},
      {:poison, "~> 3.0"},
      {:earmark, "~> 0.2.0", only: :dev},
      {:ex_doc, "~> 0.12", only: :dev},
      {:credo, "~> 0.5", only: [:dev, :test]},
-     {:plug, "~> 1.3", only: :test},
+     {:plug, "~> 1.6", only: :test},
      {:cowboy, "~> 1.0.0", only: :test}
    ]
   end

5cf349fd0800f635b282fc30c137eb2a92b46e88
João Ferreira <jmnsferreira@gmail.com>
2020-02-06 13:57:31 +0000

    Adds the oauth.v2.access method
    This implies a breaking change: the current module generator is mapping
    multi-level endpoint names to functions with dashes in the name. For example,
    `oauth.v2.access` becomes `Slack.Web.Oauth.v2/access/3`.
    
    This, while valid, makes it very dificult to call the function and I believe was
    unintended. This PR changes the generator so that the above endpoint becomes
    `Slack.Web.Oauth.V2.access/3`.

--- test/slack/web/documentation_test.exs
+++ test/slack/web/documentation_test.exs
@@ -8,4 +8,48 @@ defmodule Slack.Web.DocumentationTest do
     argument_value_keyword_list = Documentation.arguments_with_values(doc)
     assert argument_value_keyword_list === [text: {:text, [], nil}, channel: {:channel, [], nil}]
   end
+
+  describe "new/2" do
+    test "takes a documentation and filename, returns a module & function description" do
+      file_content = %{
+        "desc" => "Gets information about the current team.",
+        "args" => %{},
+        "errors" => %{}
+      }
+
+      doc = Documentation.new(file_content, "team.info.json")
+
+      assert doc.module == "team"
+      assert doc.endpoint == "team.info"
+      assert doc.function == :info
+      assert doc.desc == "Gets information about the current team."
+      assert doc.required_params == []
+      assert doc.optional_params == []
+      assert doc.errors == %{}
+      assert doc.raw == file_content
+
+      module_functions = Slack.Web.Team.__info__(:functions)
+
+      assert {:info, 0} in module_functions
+      assert {:info, 1} in module_functions
+    end
+
+    test "accepts versioned endpoints" do
+      file_content =
+        "#{__DIR__}/../../../lib/slack/web/docs/oauth.v2.access.json"
+        |> File.read!()
+        |> Poison.Parser.parse!(%{})
+
+      doc = Documentation.new(file_content, "oauth.v2.access.json")
+
+      assert doc.module == "oauth.v2"
+      assert doc.endpoint == "oauth.v2.access"
+      assert doc.function == :access
+
+      module_functions = Slack.Web.Oauth.V2.__info__(:functions)
+
+      assert {:access, 3} in module_functions
+      assert {:access, 4} in module_functions
+    end
+  end
 end
